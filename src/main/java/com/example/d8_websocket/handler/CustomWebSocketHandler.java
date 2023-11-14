e52fde71d8b35e1041cccca85dda048defbd5b29
magsut <magsut.yangaboyev@zip24.com>
2023-11-14 15:29:24 +0500

    binary

--- src/main/java/com/example/d8_websocket/handler/CustomWebSocketHandler.java
+++ src/main/java/com/example/d8_websocket/handler/CustomWebSocketHandler.java
@@ -1,11 +1,20 @@
 package com.example.d8_websocket.handler;
 
 import com.example.d8_websocket.dto.WSMsg;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
+
+import java.awt.image.DataBufferByte;
+import java.io.ObjectOutputStream;
+import java.nio.ByteBuffer;
+import java.util.function.Function;
 
 @Component
 public class CustomWebSocketHandler implements WebSocketHandler {
@@ -19,7 +28,11 @@ public class CustomWebSocketHandler implements WebSocketHandler {
 
     @Override
     public Mono<Void> handle(WebSocketSession session) {
-        var f = sink.asFlux().map(s -> session.textMessage(s.toString()));
+        Flux<WebSocketMessage> f =
+                sink.asFlux()
+                        .map(s -> session
+                                .binaryMessage(dataBufferFactory ->
+                                        dataBufferFactory.wrap(s.toString().getBytes())));
 
         return session.send(f);
     }
